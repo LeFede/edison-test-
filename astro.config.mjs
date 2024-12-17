d9a36cd9edccf42bd1212d0393af7e98d25f87f1
LeFede <lefedeok@gmail.com>
2024-12-17 19:13:14 -0300

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import awsAmplify from "astro-aws-amplify";
+// import react from "@astrojs/react";
+// import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // integrations: [react(), tailwind()],
+  output: "static", // output: 'hybrid'
+  adapter: awsAmplify(),
+
+  image: {
+    domains: [
+      "4c6fd7a052e7730e10b5e8324446b71d.cdn.bubble.io",
+      "edison-s3.s3.us-east-1.amazonaws.com",
+    ],
+  },
+});
