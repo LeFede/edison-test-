935fd285b736fe8d55a4b01eb96f66513106a7bf
LeFede <lefedeok@gmail.com>
2024-12-19 02:03:54 -0300

    compression plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,32 @@ import { defineConfig } from "astro/config";
 import awsAmplify from "astro-aws-amplify";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import CompressionPlugin from "compression-webpack-plugin";
 // import react from "@astrojs/react";
 // import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  plugins: [
+    new CompressionPlugin({
+      algorithm: "gzip", // Puedes usar 'gzip' o 'brotli' como algoritmo
+      test: /\.(js|css|html)$/i, // Comprimir archivos JS, CSS y HTML
+      threshold: 8192, // Solo comprimir archivos mayores a 8KB
+      minRatio: 0.8, // Solo comprimir si la reducción de tamaño es al menos del 20%
+    }),
+  ],
+  build: {
+    webpack: {
+      // Usar el plugin de compresión en Webpack
+      plugins: [
+        new CompressionPlugin({
+          algorithm: "gzip", // Puedes usar 'gzip' o 'brotli' como algoritmo
+          test: /\.(js|css|html)$/i, // Comprimir archivos JS, CSS y HTML
+          threshold: 8192, // Solo comprimir archivos mayores a 8KB
+          minRatio: 0.8, // Solo comprimir si la reducción de tamaño es al menos del 20%
+        }),
+      ],
+    },
+  },
   // integrations: [react(), tailwind()],
   // output: 'hybrid'
   output: "static",
