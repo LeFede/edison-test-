f80cbc74e4af7fa3ca1b6c272a0c6de13a0bc952
LeFede <lefedeok@gmail.com>
2024-12-19 02:10:31 -0300

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import awsAmplify from "astro-aws-amplify";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import CompressionPlugin from "compression-webpack-plugin";
+import BrotliWebpackPlugin from "brotli-webpack-plugin";
 // import react from "@astrojs/react";
 // import tailwind from "@astrojs/tailwind";
 
@@ -15,13 +16,27 @@ export default defineConfig({
       threshold: 8192, // Solo comprimir archivos mayores a 8KB
       minRatio: 0.8, // Solo comprimir si la reducción de tamaño es al menos del 20%
     }),
+
+    new BrotliWebpackPlugin({
+      asset: "[path].br", // Generar archivos con extensión `.br`
+      test: /\.(js|css|html)$/i, // Comprimir archivos JS, CSS y HTML
+      threshold: 8192, // Solo comprimir archivos mayores a 8KB
+      minRatio: 0.8, // Solo comprimir si la reducción de tamaño es al menos del 20%
+    }),
   ],
   build: {
     webpack: {
-      // Usar el plugin de compresión en Webpack
+      // Usar los plugins de compresión en Webpack
       plugins: [
         new CompressionPlugin({
-          algorithm: "gzip", // Puedes usar 'gzip' o 'brotli' como algoritmo
+          algorithm: "gzip", // Esto seguirá siendo Gzip para la compresión gzip
+          test: /\.(js|css|html)$/i, // Comprimir archivos JS, CSS y HTML
+          threshold: 8192, // Solo comprimir archivos mayores a 8KB
+          minRatio: 0.8, // Solo comprimir si la reducción de tamaño es al menos del 20%
+        }),
+
+        new BrotliWebpackPlugin({
+          asset: "[path].br", // Generar archivos con extensión `.br`
           test: /\.(js|css|html)$/i, // Comprimir archivos JS, CSS y HTML
           threshold: 8192, // Solo comprimir archivos mayores a 8KB
           minRatio: 0.8, // Solo comprimir si la reducción de tamaño es al menos del 20%
