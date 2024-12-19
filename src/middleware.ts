bb715022542ee6a6248f812d22dc9680120278a7
LeFede <lefedeok@gmail.com>
2024-12-19 02:37:35 -0300

    middleware2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,12 @@
-import { defineMiddleware } from "astro:middleware";
-import { brotliCompressSync, deflateSync, gzipSync } from "node:zlib";
+import { defineMiddleware } from "astro:middleware"
 
-export const onRequest = defineMiddleware(async (context, next) => {
-  const response = await next();
-  const html = await response.text();
-
-  const headers = response.headers;
-  let res: string | Buffer;
-  if (response.headers.get("Content-Type") === "text/html") {
-    const reqEncodings =
-      context.request.headers
-        .get("accept-encoding")
-        ?.split(",")
-        .map((e) => e.trim()) ?? [];
-
-    if (reqEncodings?.includes("br")) {
-      headers.set("Content-Encoding", "br");
-      res = brotliCompressSync(html);
-    } else if (reqEncodings?.includes("gzip")) {
-      headers.set("Content-Encoding", "gzip");
-      res = gzipSync(html);
-    } else if (reqEncodings?.includes("deflate")) {
-      headers.set("Content-Encoding", "deflate");
-      res = deflateSync(html);
-    } else {
-      res = html;
-    }
-  }
-
-  return new Response(res, {
-    status: response.status,
-    headers: headers,
-  });
-});
+export const onRequest = defineMiddleware(async (_context, next) => {
+    const response = await next()
+    return new Response(response.body?.pipeThrough(new CompressionStream("gzip")), {
+        ...response,
+        headers: {
+            ...response.headers,
+            "Content-Encoding": "gzip"
+        }
+    })
+})
---
+++ src/middleware2.ts
@@ -0,0 +1,15 @@
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware(async (_context, next) => {
+  const response = await next();
+  return new Response(
+    response.body?.pipeThrough(new CompressionStream("gzip")),
+    {
+      ...response,
+      headers: {
+        ...response.headers,
+        "Content-Encoding": "gzip",
+      },
+    },
+  );
+});
