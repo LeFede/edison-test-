be97f72ed76d023f192db914120badad2555d02d
LeFede <lefedeok@gmail.com>
2024-12-20 20:20:03 -0300

    no minify?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig, envField } from "astro/config";
 import awsAmplify from "astro-aws-amplify";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import compressor from "astro-compressor";
+// import compressor from "astro-compressor";
 
 export default defineConfig({
-  integrations: [react(), tailwind(), compressor()],
+  integrations: [react(), tailwind()],
   adapter: awsAmplify(),
   output: "static", // Asegúrate de que la salida esté en la carpeta estática
   env: {
